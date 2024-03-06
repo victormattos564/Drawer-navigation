c29cc71cea0d6d26663e5cbea637f0492c1bd1c2
Victormattos564 <victormaria@aluno.senai.br>
2024-03-06 10:39:24 -0300

    Atualizações de código: Adiciona componentes Title, Contact, Home e Profile

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class Title extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Title</Text>
+      </View>
+    )
+  }
+}
---
+++ src/screens/Contact/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+import styles from "./styles";
+export default function Contact() {
+  return (
+    <View style={styles.container}>
+      <Text>Contact</Text>
+    </View>
+  );
+}
